a91fccf5a86d3054e74c27dd31f7a8ae283eb7a3
Open592 Developer <uss@open592.com>
2022-09-24 23:11:11 -0700

    Add rc and live environments
    Signed-off-by: Open592 Developer <uss@open592.com>

---
+++ rc/variables.tf
@@ -0,0 +1,29 @@
+variable "AWS_ACCESS_KEY" {
+  default     = ""
+  description = "Access key for AWS. This should NEVER be stored locally"
+  type        = string
+}
+
+variable "AWS_SECRET_KEY" {
+  default     = ""
+  description = "Secret key for AWS. This should NEVER be stored locally"
+  type        = string
+}
+
+variable "AWS_REGION" {
+  default     = "us-west-2"
+  description = "AWS region. (Defaults to 'us-west-2')"
+  type        = string
+}
+
+variable "APPLICATION_NAME" {
+  default     = "Open592"
+  description = "Application name. (Defaults to 'Open592')"
+  type        = string
+}
+
+variable "ENVIRONMENT" {
+  default     = "rc"
+  description = "Development environment. (Defaults to 'rc')"
+  type        = string
+}
