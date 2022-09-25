1e8709e9328b8cf4e105e585df0548a31ad8a67e
Open592 Developer <uss@open592.com>
2022-09-24 22:21:41 -0700

    wip: Add provider
    Signed-off-by: Open592 Developer <uss@open592.com>

---
+++ wip/provider.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region     = var.AWS_REGION
+  access_key = var.AWS_ACCESS_KEY
+  secret_key = var.AWS_SECRET_KEY
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+
+  backend "remote" {
+    # For anyone using this as a base - you will need to update these
+    organization = "Open592"
+
+    workspaces {
+      name = "Infrastructure-AWS-WIP"
+    }
+  }
+}
