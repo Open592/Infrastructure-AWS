d3813fc7153c8511f3e5bcb9ae0ff67a22a502a0
Open592 Developer <uss@open592.com>
2022-09-24 22:47:57 -0700

    wip: Add explicit types to variables
    Signed-off-by: Open592 Developer <uss@open592.com>

--- wip/variables.tf
+++ wip/variables.tf
@@ -1,24 +1,29 @@
 variable "AWS_ACCESS_KEY" {
   default     = ""
   description = "Access key for AWS. This should NEVER be stored locally"
+  type        = string
 }
 
 variable "AWS_SECRET_KEY" {
   default     = ""
   description = "Secret key for AWS. This should NEVER be stored locally"
+  type        = string
 }
 
 variable "AWS_REGION" {
   default     = "us-west-2"
   description = "AWS region. (Defaults to 'us-west-2')"
+  type        = string
 }
 
 variable "APPLICATION_NAME" {
   default     = "Open592"
   description = "Application name. (Defaults to 'Open592')"
+  type        = string
 }
 
 variable "ENVIRONMENT" {
   default     = "wip"
   description = "Development environment. (Defaults to 'wip')"
+  type        = string
 }
