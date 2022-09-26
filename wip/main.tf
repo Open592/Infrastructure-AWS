3fc08c2cc931288033fcb7da2f8b780be61da04a
Open592 Developer <uss@open592.com>
2022-09-26 01:30:49 -0700

    wip: Fix undefined variable
    Signed-off-by: Open592 Developer <uss@open592.com>

--- wip/main.tf
+++ wip/main.tf
@@ -6,7 +6,7 @@ resource "aws_route53_record" "wip-ns" {
   name    = aws_route53_zone.wip-zone.name
   ttl     = 30
   type    = "NS"
-  zone_id = var.ROOT_HOSTED_ZONE_ID
+  zone_id = var.AWS_HOSTED_ZONE_ID
   records = aws_route53_zone.wip-zone.name_servers
 }
 
@@ -15,5 +15,5 @@ module "network" {
 
   ENVIRONMENT        = var.ENVIRONMENT
   ENVIRONMENT_DOMAIN = aws_route53_zone.wip-zone.name
-  HOSTED_ZONE_ID     = var.ROOT_HOSTED_ZONE_ID
+  HOSTED_ZONE_ID     = var.AWS_HOSTED_ZONE_ID
 }
