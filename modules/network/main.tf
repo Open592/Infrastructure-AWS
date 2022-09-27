6155e29b8398936832e23a4cb39b2d471af6311a
Open592 Developer <uss@open592.com>
2022-09-27 01:11:20 -0700

    wip: dash to underscore
    Signed-off-by: Open592 Developer <uss@open592.com>

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -5,7 +5,7 @@
 # cdn.open592.com
 # cdn.rc.open592.com
 # cdn.wip.open592.com
-resource "aws_route53_zone" "cdn-zone" {
+resource "aws_route53_zone" "cdn_zone" {
   name = "cdn.${var.ENVIRONMENT_DOMAIN}"
 
   tags = {
@@ -13,10 +13,10 @@ resource "aws_route53_zone" "cdn-zone" {
   }
 }
 
-resource "aws_route53_record" "cdn-ns" {
-  name    = aws_route53_zone.cdn-zone.name
+resource "aws_route53_record" "cdn_ns" {
+  name    = aws_route53_zone.cdn_zone.name
   ttl     = 30
   type    = "NS"
   zone_id = var.HOSTED_ZONE_ID
-  records = aws_route53_zone.cdn-zone.name_servers
+  records = aws_route53_zone.cdn_zone.name_servers
 }
--- wip/main.tf
+++ wip/main.tf
@@ -1,19 +1,19 @@
-resource "aws_route53_zone" "wip-zone" {
+resource "aws_route53_zone" "wip_zone" {
   name = var.ENVIRONMENT_DOMAIN
 }
 
-resource "aws_route53_record" "wip-ns" {
-  name    = aws_route53_zone.wip-zone.name
+resource "aws_route53_record" "wip_ns" {
+  name    = aws_route53_zone.wip_zone.name
   ttl     = 30
   type    = "NS"
   zone_id = data.terraform_remote_state.root_domain.outputs.root_zone_id
-  records = aws_route53_zone.wip-zone.name_servers
+  records = aws_route53_zone.wip_zone.name_servers
 }
 
 module "network" {
   source = "../modules/network"
 
   ENVIRONMENT        = var.ENVIRONMENT
-  ENVIRONMENT_DOMAIN = aws_route53_zone.wip-zone.name
+  ENVIRONMENT_DOMAIN = aws_route53_zone.wip_zone.name
   HOSTED_ZONE_ID     = data.terraform_remote_state.root_domain.outputs.root_zone_id
 }
